f60ff7cdc4c6793ec70d117e832410c84e1b7ffc
Isabelly <isabelly.paganini@gmail.com>
2020-04-21 16:00:25 -0300

    criado metodos Fibonacci e isFibonacci

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-'use strict'
+"use strict";
 
-const fibonacci = () => null
+const fibonacci = () => {
+    var numFibonacci = [];
+    var num1 = 0;
+    var num2 = 1;
+    var num3 = 0;
 
-const isFibonnaci = (num) => null
+    do {
+        numFibonacci.push(num3);
+        num1 = num1;
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    } while (num2 <= 377);
+
+    return numFibonacci;
+};
+
+const isFibonnaci = (num) => fibonacci().includes(num);
 
 module.exports = {
     fibonacci,
-    isFibonnaci
-}
+    isFibonnaci,
+};
